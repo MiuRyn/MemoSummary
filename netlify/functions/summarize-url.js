7d85a902cc019a37e1aad8fe75c71c6cbdea004c
MiuRyn <jess.yin.jy@gmail.com>
2026-06-07 14:53:32 +0800

    Update summarize-url.js

--- netlify/functions/summarize-url.js
+++ netlify/functions/summarize-url.js
@@ -1,4 +1,4 @@
-const pdfjsLib = require("pdfjs-dist/legacy/build/pdf");
+const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
 
 export async function extractPdfTextFromFirstPages(arrayBuffer, maxPages = 2) {
     const loadingTask = pdfjsLib.getDocument({
